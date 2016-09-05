e25056e539a1c57510d87f5e64e8951de8007f7c
Gogs <gogs@fake.local>
2016-09-05 20:04:02 +0800

    add business.

--- registry.hpp
+++ registry.hpp
@@ -1,19 +1,53 @@
 #pragma once
 #include <string>
+#include <map>
+#include <mutex>
 class registry
 {
 public:
 
 	bool register_service(const std::string& service_name, const std::string& host_name, int port)
 	{
-
+		std::unique_lock<std::mutex> lock;
+		map_.emplace(service_name, entity{ host_name, port });
 		return false;
 	}
 
 	bool unregister_service(const std::string& service_name, const std::string& host_name, int port)
 	{
-
+		std::unique_lock<std::mutex> lock;
+		for (auto it = map_.cbegin(); it != map_.end();)
+		{
+			if (it->first == service_name && it->second == entity{ host_name ,port})
+			{
+				it = map_.erase(it);
+			}
+			else
+			{
+				++it;
+			}
+		}
 		return false;
 	}
+
+private:
+	struct entity
+	{
+		std::string host_name;
+		int port;
+
+		bool operator ==(const entity& e) const
+		{
+			return host_name == e.host_name&&port == e.port;
+		}
+
+		bool operator !=(const entity& e) const
+		{
+			return host_name != e.host_name||port == e.port;
+		}
+	};
+
+	std::multimap<std::string, entity> map_;
+	std::mutex mtx_;
 };
 
